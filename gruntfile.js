87bdd9026a034cc1f5b16a626f46778427e74389
reallyslimy <jesusvelezmar@gmail.com>
2022-07-30 21:08:49 +0200

    Deleted minified code

--- gruntfile.js
+++ gruntfile.js
@@ -1,6 +1,7 @@
 module.exports = function(grunt){
 
     grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-copy');
 
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
@@ -14,11 +15,22 @@ module.exports = function(grunt){
             minifyJs: {
                 files: [{
                     expand: true,
-                    src: ['./**/*.js', '!./**/*.min.js', '!./node_modules/**/*'],
+                    src: ['./**/*.js', '!./**/*.min.js', '!./node_modules/**/*', '!./gruntfile.js'],
                     ext: '.min.js',
-                    dest: 'minCode/'
+                    dest: '../minCode/'
                 }]
             }
-        } 
+        },
+        copy: {
+            main: {
+                files: [
+                    {
+                        expand: true,
+                        src: ['./gruntfile.js', './LICENSE', './package-lock.json', './package.json', './README.md', './SECURITY.md', './.github/**/*'],
+                        dest: '../minCode/'
+                    }
+                ]
+            }
+        }
     })
 } 
